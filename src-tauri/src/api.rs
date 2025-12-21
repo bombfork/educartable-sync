54398688c08f539a7ec32db774f4d96959860b4d
Thomas Nemer <thomas.nemer@fortytwo.fr>
2025-12-21 20:38:33 +0100

    feat: add http api client

--- src-tauri/src/api.rs
+++ src-tauri/src/api.rs
@@ -1,2 +1,47 @@
 // API client for Educartable endpoints
-// TODO: Implement API client
+use reqwest::Client;
+use serde::{Deserialize, Serialize};
+
+pub struct EducartableClient {
+    client: Client,
+    access_token: String,
+}
+
+impl EducartableClient {
+    pub fn new(access_token: String) -> Self {
+        Self {
+            client: Client::new(),
+            access_token,
+        }
+    }
+
+    async fn get<T: for<'de> Deserialize<'de>>(
+        &self,
+        url: &str
+    ) -> Result<T, Box<dyn std::error::Error>> {
+        let response = self.client
+            .get(url)
+            .header("Authorization", &self.access_token)  // NO "Bearer"!
+            .send()
+            .await?;
+
+        let data: T = response.json().await?;
+        Ok(data)
+    }
+
+    async fn post<T: for<'de> Deserialize<'de>, B: Serialize>(
+        &self,
+        url: &str,
+        body: &B
+    ) -> Result<T, Box<dyn std::error::Error>> {
+        let response = self.client
+            .post(url)
+            .header("Authorization", &self.access_token)
+            .json(body)
+            .send()
+            .await?;
+
+        let data: T = response.json().await?;
+        Ok(data)
+    }
+}
